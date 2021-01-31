e551f4646a4cec150e0970784c9ff32651dfa51f
LeahKang <uijeng3051@gmail.com>
2021-01-31 23:59:47 +0900

    edit ...

--- week3_완전탐색/2210_숫자판점프/의정_숫자판점프.py
+++ week3_완전탐색/2210_숫자판점프/의정_숫자판점프.py
@@ -2,7 +2,7 @@
 
 # 2210번 숫자판 점프
 def dfs(x, y, number):
-    if len(number) == 6: #6자리 숫자가 만들어졌다면
+    if len(number) == 8: #8자리 숫자가 만들어졌다면
         if number not in result: #result에 없다면
             result.append(number)
         return
@@ -13,14 +13,15 @@ def dfs(x, y, number):
         ddx = x + dx[k]
         ddy = y + dy[k]
         
-        if 0 <= ddx < 5 and 0 <= ddy < 5: #범위 내에 있다면
-            dfs(ddx, ddy, number + matrix[ddx][ddy]) #6글자가 될 때 까지 재귀
+        if 0 <= ddx < 4 and 0 <= ddy < 4: #범위 내에 있다면
+            dfs(ddx, ddy, number + matrix[ddx][ddy]) #8글자가 될 때 까지 재귀
 
 #입력
-matrix = [list(map(str, input().split())) for _ in range(5)]
+matrix = [list(map(str, input().split())) for _ in range(4)]
 
 result = []
-for i in range(5):
-    for j in range(5):
-        dfs(i, j, matrix[i][j]) #0,0부터 4,4까지 모두 검사
+for i in range(4):
+    for j in range(4):
+        dfs(i, j, matrix[i][j]) #0,0부터 3,3까지 모두 검사
+# print(result)
 print(len(result))
