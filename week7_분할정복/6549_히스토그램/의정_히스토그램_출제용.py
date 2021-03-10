11ef1f06feb9b8cd338311d2807d50cd138bfe0e
LeahKang <uijeng3051@gmail.com>
2021-03-10 16:10:42 +0900

    문제 해답

---
+++ week7_분할정복/6549_히스토그램/의정_히스토그램_출제용.py
@@ -0,0 +1,54 @@
+def big_square(start, end):
+    global hist
+
+    if end-start == 1:
+        answer = hist[start]
+        return answer
+
+    half = int((start + end)/2)
+    #left
+    left = big_square(start, half)
+    #mid
+    midl, midr, width = half-1, half, 2
+    mid = min(hist[midl], hist[midr]) * width
+    while True:
+        if midl-1 < start:
+            break
+
+        midl -= 1
+        width += 1
+
+        new_mid = min(hist[midl:midr+1]) * width
+
+        if mid > new_mid:
+            break
+
+        mid = new_mid
+
+    while True:
+        if midr+1 >= end:
+            break
+        
+        midr += 1
+        width += 1
+        new_mid = min(hist[midl:midr+1]) * width
+
+        if mid > new_mid:
+            break
+
+        mid = new_mid
+
+    #right
+    right = big_square(half, end)
+    
+    answer = max([left, right, mid])
+    return answer
+
+num = int(input())
+hist = [int(input()) for _ in range(num)]
+
+answer = big_square(0, num)
+
+print(answer)
+
+
